3d0ac2544a7b9aaf04599c46bc83eae11cb24199
offlinenow <offlinenow@users.noreply.github.com>
2017-09-13 16:24:45 +0800

    intent

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myapplication.MainActivity";
@@ -14,7 +16,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, EXTRA_MESSAGE);
+        intent.setType("text/plain");
 
+        String title = getResources().getString(R.string.login);
+
+        Intent chooser = Intent.createChooser(intent, title);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        } else {
+            Toast.makeText(MainActivity.this, "请先安装相应的应用", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void PopUpWindow(View view) {
